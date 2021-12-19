1cd8f9b93f5c440b6f71b7838054e218c153623e
Tony Avis <ttavis1999@gmail.com>
2021-12-19 18:52:17 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const video = document.getElementById('play');
+console.log(video);
+
+
+function hover (){
+    video.play();
+    console.log(video);
+}
+
+
+function stop (){
+    video.pause();
+    console.log(video);
+}
